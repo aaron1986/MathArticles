fd74b0bb355e0ea87e150757646a96d783e7f6b3
aaron <aaronbrucesmith86@gmail.com>
2025-12-30 17:32:13 +0000

    updated contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -22,17 +22,17 @@ export default function Contact() {
             <form className="contact-form" name="contact" method="POST" data-netlify="true">
               <input type="hidden" name="form-name" value="contact" />
 
-            <div className="form_group">
+            <div className="form-group">
           <label htmlFor="name">Name</label>
           <input id="name" name="name" value={formData.name} onChange={handleChange} required />
         </div> {/* end div for form_group */}
              
-            <div className="form_group">
+            <div className="form-group">
           <label htmlFor="email">Email</label>
           <input id="email" name="email" type="email" value={formData.email} onChange={handleChange} required />
         </div> {/* end div for form_group */}
 
-            <div className="form_group">
+            <div className="form-group">
           <label htmlFor="message">Message</label>
           <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
         </div> {/* end div for form_group */}
