36a343f14363a034d33a1503593103e833c3ffb1
aaron <aaronbrucesmith86@gmail.com>
2025-12-30 17:00:37 +0000

    fixed contact page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,14 @@
 export default function About() {
     return (
+       <>
         <div className="article_title">About</div>
+        <div className="about-container">
+            <div>
+                <p>
+                    This website serves as a small demonstration of my technical skills and showcases selected mathematics work which I completed during my studies at College.
+                </p>
+            </div>
+        </div> {/* end of about-container div */}
+       </>
     )
 }
