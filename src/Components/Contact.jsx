fbfb93307061c6e62a9f00496fba3eec669bda34
aaron <aaronbrucesmith86@gmail.com>
2025-12-30 17:30:57 +0000

    updated contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -15,37 +15,26 @@ export default function Contact() {
     }));
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    //test in console!
-    console.log(formData);
-
-    setFormData({
-      name: "",
-      email: "",
-      message: ""
-    });
-  };
-
     return (
         <>
         <div className="article_title">Contact Page</div>
         <div className="contact-container">
-                <form onSubmit={handleSubmit} className="contact-form">
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required placeholder="Your Name..." />
-            </div> {/* end div for form_group */}
+            <form className="contact-form" name="contact" method="POST" data-netlify="true">
+              <input type="hidden" name="form-name" value="contact" />
 
-             <div className="form-group">
-          <label htmlFor="email">Email *</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required placeholder="your.email@example.com" />
+            <div className="form_group">
+          <label htmlFor="name">Name</label>
+          <input id="name" name="name" value={formData.name} onChange={handleChange} required />
+        </div> {/* end div for form_group */}
+             
+            <div className="form_group">
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" value={formData.email} onChange={handleChange} required />
         </div> {/* end div for form_group */}
 
-        <div className="form-group">
-          <label htmlFor="message">Message *</label>
-          <textarea id="message" name="message" value={formData.message} onChange={handleChange} required rows="6" placeholder="Tell us more about your inquiry..." />
+            <div className="form_group">
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" value={formData.message} onChange={handleChange} required />
         </div> {/* end div for form_group */}
 
         <button type="submit" className="submit-btn">Send Message</button>
