26852084641b5a522a4988d3e6aa71d0bf7d2c67
aaron <aaronbrucesmith86@gmail.com>
2026-01-01 14:57:07 +0000

    updated success page

--- src/Components/Success.jsx
+++ src/Components/Success.jsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom";
 export default function Success() {
+  const navigate = useNavigate();
     return (
           <div>
             <div className="article_title">Thank you!</div>
             <p>Your message has been sent successfully.</p>
+            <div>
+             <button onClick={() => navigate("/")}>
+            Return Home
+          </button>
+            </div>
           </div>
 
     )
